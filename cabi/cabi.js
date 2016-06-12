e64a5ae1cc4fd0ed06f2ffaa71c568ee239d84a6
derickson <david.erickson@gmail.com>
2016-06-12 16:14:08 -0400

    cabi service

---
+++ cabi/cabi.js
@@ -0,0 +1,129 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: Cabi
+ * 
+ * Shows the capital bikeshare stations available
+ *
+ * By @derickson
+ * based on MagicMirror work by Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+Module.register("cabi",{
+
+	// Default module config.
+	defaults: {
+		stationName: null,
+
+		updateInterval: 1 * 60 * 1000, // every 5 minutes
+		animationSpeed: 1000,
+		timeFormat: config.timeFormat,
+
+		initialLoadDelay: 0, // 0 seconds delay	
+		retryDelay: 10000
+	},
+
+	// Define required scripts.
+	getScripts: function() {
+		return ["moment.js"];
+	},
+
+	// Define required styles.
+	getStyles: function() {
+		return ['cabi.css'];
+	},
+
+	start: function() {
+		Log.info("Starting module: " + this.name);
+
+		// Set locale.
+		moment.locale(config.language);
+
+		// variables that will be loaded from service
+		this.numBikes = null;
+		this.numFreeSlots = null;
+
+		// start the timer
+		this.loaded = false;
+		
+		this.scheduleUpdate(0);
+		this.updateTimer = null;
+
+	
+
+	},
+
+	// start load timer (with posibility of delay)
+	scheduleUpdate: function(delay) {
+		var nextLoad = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
+		}
+
+		var self = this;
+		setTimeout(function() {
+			self.updateCabi();
+		}, nextLoad);
+	},
+
+	// reach out to cabi service
+	updateCabi: function() {
+	
+		var self = this;
+		self.sendSocketNotification("START_CABI", {stationName: self.config.stationName});
+
+	},
+
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === "CABI_DATA_RESPONSE") {
+			console.log('cabi received message for data call');
+			this.numBikes = payload.numBikes;
+			this.numFreeSlots = payload.numFreeSlots;
+			this.processCabi(null);
+			this.scheduleUpdate(-1);
+		}
+	},
+
+
+
+	// unload the results from cabi services
+	processCabi: function(result) {
+		console.log("ProcessCabi");
+		console.log(result);
+
+		// when done, redraw the module
+		this.loaded = true;
+		this.updateDom(this.config.animationSpeed);
+	},
+
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+
+		var cabi = document.createElement("div");
+		cabi.className = "cabiContainer";
+		
+		var cabiIcon = document.createElement("img");
+		cabiIcon.className = "badge";
+		cabiIcon.src = "modules/derickson/cabi/cabi_small.png";
+
+		var cabiText = document.createElement("span");
+
+		if(this.loaded) {
+			cabiText.innerHTML = this.config.stationName + ": " + this.numBikes + " Bikes and "+  this.numFreeSlots +" Free Bike Slots";
+		} else {
+			// Loading message
+			cabiText.innerHTML = "Checking Cabi status ...";
+		}
+		
+
+		cabi.appendChild(cabiIcon);
+		cabi.appendChild(cabiText);
+		
+		wrapper.appendChild(cabi);
+		return wrapper;
+	}
+
+});
---
+++ cabi/node_helper.js
@@ -0,0 +1,65 @@
+/* Magic Mirror
+ * Node Helper: Calendar
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+var NodeHelper = require("node_helper");
+
+var request = require("request");
+var xml2js = require('xml2js');
+
+module.exports = NodeHelper.create({
+	// Override start method.
+	start: function() {
+		var self = this;
+		
+		console.log("Starting node helper for: " + this.name);
+
+	},
+
+	// Override socketNotificationReceived method.
+	socketNotificationReceived: function(notification, payload) {
+		var self = this;
+
+		if(notification === "START_CABI"){
+			console.log('Node helper got message start cabi');
+			self.fetch(payload.stationName, function(station) {
+				self.sendSocketNotification("CABI_DATA_RESPONSE", 
+					{
+						numBikes: parseInt(station.nbBikes[0]), 
+						numFreeSlots: parseInt(station.nbEmptyDocks[0])
+					}
+				);
+			});
+		}
+
+	},
+
+
+	fetch: function(stationName, cb) {
+		request( "https://www.capitalbikeshare.com/data/stations/bikeStations.xml", function(error, response, body){
+			if(!error && response.statusCode === 200) {
+				xml2js.parseString(body, function(err, result){
+					if(!err){
+						var stationArray = result.stations.station;
+						for( k in stationArray ){
+							var station = stationArray[k];
+							
+							if(station.name[0] === stationName){
+								cb(station);
+								break;
+							}
+						}
+					} else {
+						console.log(err);
+					}
+				});
+			} else {
+				console.log("Error in request to cabi service");
+			}
+		});
+	}
+
+});
